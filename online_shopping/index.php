de70acd99eaf680d8410e20d095a4168c16c927c
sakshi kulshreshtha <kulshreshthasakshi631@gmail.com>
2021-07-22 23:52:16 +0530

    first commit

---
+++ online_shopping/index.php
@@ -0,0 +1,147 @@
+<html>
+	<head>
+		<title>Online Shopping</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		<?php include 'includes/header.php'; ?>
+		<div class="container">
+		<div class="col-lg-12">
+                     <div class="carousel slide" id="carousel1" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                            <li data-target="#carousel1" data-slide-to="0"></li>
+                            <li data-target="#carousel1" data-slide-to="1"></li>
+                            <li data-target="#carousel1" data-slide-to="2"></li>
+                            <li data-target="#carousel1" data-slide-to="3"></li>
+                            <li data-target="#carousel1" data-slide-to="4"></li>
+                            <li data-target="#carousel1" data-slide-to="5"></li>
+                            <li data-target="#carousel1" data-slide-to="6"></li>
+                            </ol>
+                     <div class="carousel-inner">
+                            <div class="item active">
+							    
+                                 <img src="images/sale-1.jpg" class="carousel_img">
+                                 <div class="carousel-caption">
+                                 <h2>Hurry up!! Grab the offers</h2>
+								 </div>
+                                 
+                            </div>
+                            <div class="item">
+                                  <img src="images/sale-2.jpg" class="carousel_img">
+                                  <div class="carousel-caption">
+                                  <h2>Hurry up!! Grab the offers</h2>
+                                  </div>
+				            </div>
+                            <div class="item">
+                                  <img src="images/sale-3.jpg" class="carousel_img">
+                                  <div class="carousel-caption">
+                                  <h2>Hurry up!! Grab the offers</h2>
+                                  </div>
+                            </div>
+                            <div class="item">
+                                 <img src="images/sale-4.jpg" class="carousel_img">
+                                 <div class="carousel-caption">
+                                 <h2>Hurry up!! Grab the offers</h2>
+                                 </div>
+                            </div>
+                            <div class="item">
+                                 <img src="images/sale-5.jpg" class="carousel_img">
+                                 <div class="carousel-caption">
+                                 <h2>Hurry up!! Grab the offers</h2>
+                                 </div>
+				            </div>
+                           
+     
+                           <a href="#carousel1" data-slide="prev" class="left carousel-control"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                           <a href="#carousel1" data-slide="next" class="right carousel-control"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                        </div>
+	               </div>
+			<div class="row">
+				<div class="col-lg-12">
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/watch.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Wrist Watches</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 499</div>
+						</div>
+					</div>
+				</div><div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/men-clothing.jpg"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product1.php">Men Western Wear</a></h3>
+							
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 299</div>
+						</div>
+					</div>
+				</div><div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/men-ethnic.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product2.php">Men Ethnic Wear</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 499</div>
+						</div>
+					</div>
+				</div><div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/sportswear.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product3.php">Sportswear</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 399</div>
+						</div>
+					</div>
+				</div><div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/women-westernwear.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product4.php">Women Tops</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 199</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/women-dresses.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product5.php">Women Dresses</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 399</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/women-ethnic.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product6.php">Women Ethnic wear</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 349</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/groceries.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product7.php">Groceries</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Flat 20% discount</div>
+						</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div><div class="clearfix"></div>
+		<?php include 'includes/footer.php'; ?>
+	</body>
+</html>
---
+++ online_shopping/product4.php
@@ -0,0 +1,117 @@
+<html>
+	<head>
+		<title>Product detail</title>
+		
+		<link rel="stylesheet" href="css/font-awesome.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<style>
+			.btn {
+				font-size: 40px;
+				height: 70px;
+			}
+		</style>
+	</head>
+	<body>
+		<?php include 'includes/header.php'; ?>
+		<div class="container">
+			<div class="row">
+				<ol class="breadcrumb">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Women Tops</a></li>
+					<li class="active">Tops</li>
+				</ol>
+			</div>
+			<div class="row">
+			<div class="col-md-8">
+				<h3 class="pp-title">Stylish top</h3>
+				<img src="images/products/top-buy.jpg" class="img-responsive">
+				<h4 class="pp-desc-head">Description</h4>
+				<div class="pp-desc-detail">
+					<p>This is a very beautiful product which is worth buying and will enhance your wardrobe.</p>
+					<ul>
+						<li>It's Beautiful and stretchable</li>
+						<li>Amazing quality</li>
+						<li>Ranked 1 in best-sellers</li>
+						<li>Rated 4.5. out of by 236 customers</li>
+						<li>Pay Securely via <b>CASH ON DELIVERY</b> method</li>
+					</ul>
+				</div>
+			</div>
+			<aside class="col-md-4">
+				
+				<a href="buy.php" class="btn btn-success btn-lg btn-block">Buy</a>
+				<br>
+				<ul class="list-group">
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-md-3"><i class="fa fa-truck fa-2x"></i></div>
+							<div class="col-md-9">Delivered within 5 days</div>
+						</div>
+					</li>
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-md-3"><i class="fa fa-refresh fa-2x"></i></div>
+							<div class="col-md-9">Easy return in 7 days</div>
+						</div>
+					</li>
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-md-3"><i class="fa fa-phone fa-2x"></i></div>
+							<div class="col-md-9">Call at 123456789</div>
+						</div>
+					</li>
+				</ul>
+			</aside>
+			</div>
+			<div class="page-header">
+				<h2>Related Items</h2>
+			</div>
+			<section class="row">
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/sportswear.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Sportswear</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 399</div>
+						</div>
+					</div>
+				</div><div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/women-westernwear.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Women Tops</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 199</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/women-dresses.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Women Dresses</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 399</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3">
+					<div class="col-lg-12  single-item noPadding">
+						<div class="top"><img src="images/products/women-ethnic.jfif"></div>
+						<div class="bottom">
+							<h3 class="item-title"><a href="product.php">Women Ethnic wear</a></h3>
+							<div class="clearfix"></div>
+							<div class="pull-right discounted-price">Starting at Rs. 349</div>
+						</div>
+				
+				</div>
+		
+			</section>
+		</div><br><br><br><br><br>
+		<?php include 'includes/footer.php'; ?>
+	</body>
+</html>
